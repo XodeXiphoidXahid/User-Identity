1797e5adf03f7740e9ea53e6300918c5f8d0ffb9
Zahid Sheikh <48240960+XodeXiphoidXahid@users.noreply.github.com>
2022-04-09 13:52:51 +0600

    Add validation in Registration Form

--- UI/src/app/components/register/register.component.ts
+++ UI/src/app/components/register/register.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup } from '@angular/forms';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
@@ -8,20 +8,40 @@ import { FormControl, FormGroup } from '@angular/forms';
 })
 export class RegisterComponent implements OnInit {
 
+  repeatPass: string='none';
   constructor() { }
 
   ngOnInit(): void {
   }
 
   registerForm= new FormGroup({
-    email: new FormControl(""),
-    pwd: new FormControl(""),
+    email: new FormControl("", [Validators.required, Validators.email]),
+    pwd: new FormControl("", 
+    [Validators.required,
+      Validators.minLength(6),
+      Validators.maxLength(15),
+    ]),
     cPwd: new FormControl("")
   });
 
   registerSubmitted(){
-    console.log(this.registerForm);
+    if(this.Pwd.value==this.Cpwd.value){
+      console.log("Submitted");
+      this.repeatPass='none'
+    }else{
+      this.repeatPass='inline'
+    }
   }
 
+  get Email(): FormControl{
+    return this.registerForm.get("email") as FormControl;
+  }
+
+  get Pwd(): FormControl{
+    return this.registerForm.get("pwd") as FormControl;
+  }
+  get Cpwd(): FormControl{
+    return this.registerForm.get("cPwd") as FormControl;
+  }
 }
 
